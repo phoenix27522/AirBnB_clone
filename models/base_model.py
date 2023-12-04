e22bf2c626bc5efc1824b22ca1118e72c62d3521
Nahom kiflu <kiflunahom1994@gmail.com>
2023-12-05 01:31:35 +0300

    done with task-4 N.B:- no unit test is done for this task

--- models/base_model.py
+++ models/base_model.py
@@ -12,40 +12,50 @@ class BaseModel:
 
     Attributes:
         id (int): Identifier for the model instance.
-        created_at (datetime): Timestamp indicating when the model instance was created.
-        updated_at (datetime): Timestamp indicating when the model instance was last updated.
+        created_at (datetime): Timestamp indicating when
+                               the model instance was created.
+        updated_at (datetime): Timestamp indicating when the
+                               model instance was last updated.
     """
 
-    def __init__(self, id=None, created_at=None, updated_at=None):
-        """Initialize a BaseModel instance.
+    def __init__(self, *args, **kwargs):
+        """Initialization of a Base instance.
 
         Args:
-            id (int): Identifier for the model instance.
-            created_at (datetime, optional): Timestamp indicating when the model instance was created.
-                                             Defaults to the current time if not provided.
-            updated_at (datetime, optional): Timestamp indicating when the model instance was last updated.
-                                             Defaults to the current time if not provided.
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
         """
-
-        self.id = id or str(uuid.uuid4())
-        self.created_at = created_at or datetime.now()
-        self.updated_at = updated_at or datetime.now()
+        if kwargs is not None and kwargs != {}:
+            kwargs.pop('__class__', None)
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    setattr(self, "created_at", datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "updated_at":
+                    setattr(self, "updated_at", datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = id or str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-         """Base class defining common attributes/methods for other classes."""
+        """string representation of the calss"""
 
-         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Updates the 'updated_at' attribute with the current datetime."""
 
         self.updated_at = datetime.now()
 
-
     def to_dict(self):
         """Return a dictionary representation of the instance."""
+
         base_dict = self.__dict__.copy()
-        base_dict['__class__'] = self.__class__.__name__  # Ensure '__class__' is in quotes
+        base_dict['__class__'] = self.__class__.__name__
         base_dict['created_at'] = base_dict['created_at'].isoformat()
         base_dict['updated_at'] = base_dict['updated_at'].isoformat()
         return base_dict
