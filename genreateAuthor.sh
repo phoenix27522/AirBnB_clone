c65fc95f1da7bb413a07824d8093f0d476462cce
Nahom kiflu <kiflunahom1994@gmail.com>
2023-12-04 14:14:29 +0300

    fixed the generating authors

--- genreateAuthor.sh
+++ genreateAuthor.sh
@@ -2,20 +2,12 @@
 
 OUTPUT_FILE="AUTHORS"
 
-github_users=()
-
-echo "# The following people wrote this simple_shell" > "$OUTPUT_FILE"
-
+echo "# The following people wrote this AirBnB_clone" > "$OUTPUT_FILE"
 echo "" >> "$OUTPUT_FILE"
 
-# Use Git log to get a list of email addresses from the commit history
-git shortlog -se \
-  | perl -spe 's/^\s+\d+\s+//' \
-  | sed -e '/^CommitSyncScript.*$/d' \
-  | grep -oE '<[^>]*>' \
-  | sed 's/[<>]//g' \
-  | sed 's/[+]//g' \
-  | sed 's/^[0-9]*//'\
-  >> "$OUTPUT_FILE"
+# Use Git log to get a list of formatted names and email addresses from the commit history
+git log --format='%aN <%aE>' | grep -v 'github.com' | sort -u >> "$OUTPUT_FILE"
+
 
 echo "List of people contributed to this repo has been generated and saved to $OUTPUT_FILE"
+
